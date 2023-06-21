a6267080b610f7513855f7992754c7722a67c0b4
Melina <mrequena@fue.undef.edu.ar>
2023-06-21 15:15:48 -0300

    first commit

---
+++ parcial.c
@@ -0,0 +1,5 @@
+//
+// Created by Administrador on 21/6/2023.
+//
+
+#include "parcial.h"
---
+++ parcial.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrador on 21/6/2023.
+//
+
+#ifndef PARCIAL260622_PARCIAL_H
+#define PARCIAL260622_PARCIAL_H
+
+#endif //PARCIAL260622_PARCIAL_H
